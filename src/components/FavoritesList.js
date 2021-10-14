e93eaedeca0ed5ffc37a50f1cad8470ae32a9772
Jess LaFrank <jesslafrank@gmail.com>
2021-10-14 10:17:31 -0700

    FavoritesList succesful fetch call and displays data in a table

--- src/components/FavoritesList.js
+++ src/components/FavoritesList.js
@@ -1,19 +1,49 @@
-import React from "react";
-import { Modal } from "react-bootstrap";
+import React, { useState, useEffect } from "react";
+import { Modal, Table } from "react-bootstrap";
 
 const FavoritesList = (props) => {
   // needs to fetch all Favorites items from the database
   // not sure if need to update state yet
+  // i think state will update when component mounts
 
+  const [favorites, setFavorites] = useState([]);
+
+  const getFavorites = async () => {
+    try {
+      const favorites = await fetch("http://localhost:9000/favorites");
+      const parsedFavorites = await favorites.json();
+      console.log(parsedFavorites);
+      setFavorites(parsedFavorites);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const favoritesRow = favorites.map((favorite) => (
+    <tr>
+      <td>{favorite.activity}</td>
+      <td>{favorite.category}</td>
+    </tr>
+  ));
+
+  useEffect(() => {
+    getFavorites();
+  }, []);
   return (
     <div>
       <Modal.Header closeButton></Modal.Header>
       <Modal.Body>
-        <ul>
-          <li>List of Favorites</li>
-          <li>List of Favorites</li>
-          <li>List of Favorites from FL</li>
-        </ul>
+        <Table responsive>
+          <table>
+            <thead>
+              <tr>
+                <th>Activity</th>
+                <th>Category</th>
+              </tr>
+            </thead>
+            <tbody>{favoritesRow}</tbody>
+          </table>
+        </Table>
       </Modal.Body>
     </div>
   );
