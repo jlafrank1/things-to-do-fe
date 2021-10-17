021ef8eb53478cf55419fd80545bbb45f390c77a
Jess LaFrank <jesslafrank@gmail.com>
2021-10-17 16:32:59 -0700

    passing currentUser as props from Results to FavResults

--- src/components/FavoriteResults.js
+++ src/components/FavoriteResults.js
@@ -8,6 +8,8 @@ const FavoriteResults = (props) => {
   const { activity, type } = useContext(DataContext);
   const { currentUser, token, BASE_URL } = useContext(LoginContext);
 
+  console.log("FAVRESULTS currentUser > ", currentUser);
+
   // form state
   const [input, setInput] = useState();
   const [form, setForm] = useState();
@@ -29,19 +31,21 @@ const FavoriteResults = (props) => {
         headers: {
           "content-type": "application/json",
           "access-control-allow-origin": "*",
-          "Authorization": `bearer ${token}`,
+          Authorization: `bearer ${token}`,
         },
       };
       const createdFavorite = await fetch(BASE_URL + "/favorites", config);
       const parsedFavorite = await createdFavorite.json();
-      console.log("FAVORITERESULTS > handleSubmit > your new favorite is > ",  parsedFavorite);
+      console.log(
+        "FAVORITERESULTS > handleSubmit > your new favorite is > ",
+        parsedFavorite
+      );
 
       if (!currentUser._id) {
-        alert("Please log in!")
+        alert("Please log in!");
       } else {
-      alert("Added to Favorites!")
+        alert("Added to Favorites!");
       }
-
     } catch (err) {
       console.log(err);
     }
@@ -55,7 +59,7 @@ const FavoriteResults = (props) => {
       isDone: false,
       creator: currentUser._id,
     });
-  }, [activity, type, currentUser]);
+  }, [activity, type, currentUser._id]);
 
   const handleChange = (e) => {
     setInput({ ...input, [e.target.name]: e.target.value });
@@ -65,10 +69,25 @@ const FavoriteResults = (props) => {
     <>
       <div id="email-form">
         <form onSubmit={handleSubmit}>
-          <input type="hidden" name="activity" value={activity} onChange={handleChange} />
-          <input type="hidden" name="category" value={type} onChange={handleChange} />
-          <input type="hidden" name="isDone" value="false" onChange={handleChange} />
-          <input type="hidden"
+          <input
+            type="hidden"
+            name="activity"
+            value={activity}
+            onChange={handleChange}
+          />
+          <input
+            type="hidden"
+            name="category"
+            value={type}
+            onChange={handleChange}
+          />
+          <input
+            type="hidden"
+            name="isDone"
+            value="false"
+            onChange={handleChange}
+          />
+          <input
             name="creator"
             value={currentUser._id}
             onChange={handleChange}
--- src/components/Results.js
+++ src/components/Results.js
@@ -2,19 +2,21 @@ import React from "react";
 import { useContext } from "react";
 import FavoriteResults from "./FavoriteResults";
 import { DataContext } from "./Filters";
-import { LoginContext } from '../App'
+import { LoginContext } from "../App";
 
 const Results = (props) => {
   const { activity, type } = useContext(DataContext);
   const { currentUser } = useContext(LoginContext);
-  console.log("RESULTS > currentUser > ", currentUser)
+  console.log("RESULTS > currentUser > ", currentUser);
   console.log("RESULTS activity, type > ", activity, type);
 
   return (
     <>
       <h2>{activity}</h2>
 
-      <div id="email-results">{activity ? <FavoriteResults /> : null}</div>
+      <div id="email-results">
+        {activity ? <FavoriteResults currentUser={currentUser} /> : null}
+      </div>
     </>
   );
 };
